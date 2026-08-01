81f2b5ca9b364d169bfddf6319ee85f71a6d3c3e
AmmoniaW <ammoniaw@outlook.com>
2026-08-01 23:23:49 +0800

    feat(little_turtle): 完成005讲-是时候讲讲代码了课后练习

---
+++ exercises/little_turtle/005-是时候讲讲代码了/compare.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Software: PyCharm
+# @Author: Ammoniaw
+# @FileName: compare.py
+# @Date: 2026/8/1
+# @Description: 比较数字
+num1 = int(input("请输入第一个整数："))
+num2 = int(input("请输入第二个整数："))
+
+if num1 < num2:
+    print("第一个数比第二个数小！")
+
+if num1 > num2:
+    print("第一个数比第二个数大！")
+
+if num1 == num2:
+    print("第一个数和第二个数一样大！")
