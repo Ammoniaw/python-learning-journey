3831a52b93164dd73b24a9eca14bcacd7d9e771c
AmmoniaW <ammoniaw@outlook.com>
2026-08-03 00:23:26 +0800

    feat(little_turtle): 完成012-013 流程图和思维导图课后练习
     - 012-流程图 代码执行的具体过程
     - 013-思维导图 整个项目或者代码设计理念 为代码设计提供基础

---
+++ exercises/little_turtle/012-流程图/triangle.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Software: PyCharm
+# @Author: Ammoniaw
+# @FileName: triangle.py
+# @Date: 2026/8/2
+# @Description: 绘制三角形
+n = int(input("请输入三角形的层数:>"))
+i = 1
+while i <= n:
+    j = 0
+    while j < n - i:
+        print(" ", end='')
+        j = j + 1
+    j = 0
+    while j < 2 * i - 1:
+        print("*", end="")
+        j = j + 1
+    print("")
+    i = i + 1
